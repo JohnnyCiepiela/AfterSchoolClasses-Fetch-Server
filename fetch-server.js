2a96b2d5d46a733f97cf85eb094df2603c3f18b8
JohnnyCiepiela <99767967+JohnnyCiepiela@users.noreply.github.com>
2023-01-29 19:10:28 +0100

    Finishing search functionality, cleanup.

--- fetch-server.js
+++ fetch-server.js
@@ -2,11 +2,9 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const path = require("path");
-
 let propertiesReader = require("properties-reader");
 let propertiesPath = path.resolve(__dirname, "conf/db.properties");
 let properties = propertiesReader(propertiesPath);
-
 let dbPprefix = properties.get("db.prefix");
 let dbUsername = encodeURIComponent(properties.get("db.user"));
 let dbPwd = encodeURIComponent(properties.get("db.pwd"));
@@ -15,15 +13,14 @@ let dbUrl = properties.get("db.dbUrl");
 let dbParams = properties.get("db.params");
 
 const uri = dbPprefix + dbUsername + ":" + dbPwd + dbUrl + dbParams;
-
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const client = new MongoClient(uri, { serverApi: ServerApiVersion.v1 });
 let db = client.db(dbName);
 
-
 let app = express();
 app.set('json spaces', 3);
 
+//Logger?
 /* app.use(function(req, res, next) {
    console.log("Incoming request: " + req.url);
    next();
@@ -36,7 +33,6 @@ app.use(cors());
 app.use(morgan("short"));
 app.use(express.json());
 
-
 app.param('collectionName', function (req, res, next, collectionName) {
    req.collection = db.collection(collectionName);
    return next();
@@ -45,15 +41,6 @@ app.param('collectionName', function (req, res, next, collectionName) {
 app.get('/', function (req, res, next) {
    res.send('Please select a collection, e.g., /collections/lessons')
 });
-
-/* app.get("/user", function(req, res) {
-
-   res.json({
-      "email": "user@email.com",
-      "password": "password"
-   });
-   
-}) */
 
 app.get('/collections/:collectionName', function (req, res, next) {
    req.collection.find({}).toArray(function (err, results) {
@@ -64,6 +51,7 @@ app.get('/collections/:collectionName', function (req, res, next) {
    });
 });
 
+//For search
 app.get('/collections/:collectionName/search', function (req, res, next) {
    const searchTerm = req.query.q;
    req.collection.find( { $or: [{ title: { $regex: new RegExp(searchTerm, 'i') } }, { location: { $regex: new RegExp(searchTerm, 'i') } }] }).toArray(function (err, results) {
@@ -73,16 +61,6 @@ app.get('/collections/:collectionName/search', function (req, res, next) {
       res.send(results);
    });
 });
-
-/* app.get('/collections/:collectionName/search', function (req, res, next) {
-   const searchTerm = req.query.q;
-   req.collection.find({ title: { $regex: new RegExp(searchTerm, 'i') } }).toArray(function (err, results) {
-      if (err) {
-         return next(err);
-      }
-      res.send(results);
-   });
-}); */
 
 //For order submit
 app.post('/collections/:collectionName', function (req, res, next) {
@@ -94,6 +72,7 @@ app.post('/collections/:collectionName', function (req, res, next) {
    });
 });
 
+//For put
 app.put('/collections/:collectionName/:id', function (req, res, next) {
    req.collection.updateOne({ _id: new ObjectId(req.params.id) },
       { $set: req.body },
@@ -106,11 +85,6 @@ app.put('/collections/:collectionName/:id', function (req, res, next) {
       }
    );
 });
-
-/*
-app.delete("/", function(req, res) {
-    res.send("Are you sure??? Ok, let’s delete a record");
-    }); */
 
 app.use(function (req, res) {
    res.status(404).send("Resource not found...");
